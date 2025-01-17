5fdb7b7028bc5f9c70d082ddd1cabc7d73fdbf4c
KAVYAJAIN410 <kavya.jain2023@vitstudent.ac.in>
2025-01-17 15:01:10 +0530

    e

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -5,7 +5,47 @@ import NextAuth, { NextAuthOptions } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import { JWT } from 'next-auth/jwt';
 import { NextResponse } from 'next/server';
-import { generateTokens } from '../../login/generateTokensUser/route';
+
+import { UserToken } from "@/models/usertoken";
+import jwt from "jsonwebtoken";
+import { Types } from "mongoose";
+
+interface User {
+  _id: Types.ObjectId;
+}
+
+interface Tokens {
+  accessToken: string;
+  refreshToken: string;
+}
+
+export async function generateTokens(user: User): Promise<Tokens> {
+  try {
+    const payload = {
+      _id: user._id,
+    };
+
+    const accessToken = jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET || "", {
+      expiresIn: "5d",
+    });
+
+    const refreshToken = jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET || "", {
+      expiresIn: "30d",
+    });
+
+    const userToken = await UserToken.findOne({ userId: user._id });
+    if (userToken) {
+      await userToken.deleteOne();
+    }
+
+    await new UserToken({ userId: user._id, token: refreshToken }).save();
+
+    return { accessToken, refreshToken };
+  } catch (err) {
+    throw err;
+  }
+}
+
 
 // OAuth2Client instance
 const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID as string);
